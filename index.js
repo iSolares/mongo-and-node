b8b5f76defe3dac59bd2d8a694f97a5bca980345
Moisés Solares Santos <moisessolaressantos@gmail.com>
2024-05-09 16:02:54 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const { faker } = require('@faker-js/faker');
+const { exec } = require('child_process')
+const fs = require('fs')
+const mongoose = require('mongoose')
+try {
+    mongoose.connect("mongodb://localhost:27017/personData")
+    console.log("Mongodb is on.")
+} catch (error) {
+    console.log(error)
+}
+const clients = []
+const nameJson = './persons.json'
+const dataBaseName = 'personsData'
+const collectionName = 'people'
+let _id = 1
+for (let i = 0; i < 5; i++) {
+    const nome = faker.person.firstName()
+    const email = faker.internet.email({firstName: nome}).toLowerCase()
+    const numeroTelefone = faker.phone.number()
+    const anoNascimento = faker.date.past({years: 30})
+    
+    let client = {
+        _id,
+        nome,
+        email,
+        numeroTelefone,
+        anoNascimento
+    }
+
+    clients.push(client)
+    fs.writeFileSync(nameJson, JSON.stringify(clients), {encoding: 'utf-8'})
+    _id++
+
+}
+exec(`mongoimport --jsonArray ${nameJson} -d ${dataBaseName} -c ${collectionName}`)
+console.log("Thats all!")
---
+++ models/user-schema.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.ObjectId,
+    nome: String,
+    email: String,
+    numeroTelefone: String,
+    anoNascimento: String
+})
