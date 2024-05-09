663a8d92aeacef9493fe3732290778d422f915aa
Moisés Solares Santos <moisessolaressantos@gmail.com>
2024-05-09 17:02:00 -0300

    make a better form

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const { faker } = require('@faker-js/faker');
-const { exec } = require('child_process')
-const fs = require('fs')
 const mongoose = require('mongoose')
 try {
     mongoose.connect("mongodb://localhost:27017/personData")
@@ -8,29 +6,30 @@ try {
 } catch (error) {
     console.log(error)
 }
-const clients = []
-const nameJson = './persons.json'
-const dataBaseName = 'personsData'
-const collectionName = 'people'
-let _id = 1
-for (let i = 0; i < 5; i++) {
-    const nome = faker.person.firstName()
-    const email = faker.internet.email({firstName: nome}).toLowerCase()
-    const numeroTelefone = faker.phone.number()
-    const anoNascimento = faker.date.past({years: 30})
-    
-    let client = {
-        _id,
-        nome,
-        email,
-        numeroTelefone,
-        anoNascimento
-    }
 
-    clients.push(client)
-    fs.writeFileSync(nameJson, JSON.stringify(clients), {encoding: 'utf-8'})
-    _id++
+ const userSchema = mongoose.Schema({
+    nome: String,
+    email: String,
+    numeroTelefone: String,
+    anoNascimento: String
+})
+
+mongoose.model("people", userSchema)
+
+
+for (let i = 0; i < 20; i++) {
+
+    try {
+        const alguem = mongoose.model('people')
+        new alguem({
+            nome: faker.person.firstName(),
+            email: faker.internet.email().toLowerCase(),
+            numeroTelefone: faker.phone.number(),
+            anoNascimento: faker.date.past({years: 30})
+        }).save()
+        } catch (error) {
+            console.log(error)
+        }
 
 }
-exec(`mongoimport --jsonArray ${nameJson} -d ${dataBaseName} -c ${collectionName}`)
 console.log("Thats all!")
--- models/user-schema.js
+++ models/user-schema.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 
-const userSchema = mongoose.Schema({
+ const userSchema = mongoose.Schema({
     _id: mongoose.Schema.ObjectId,
     nome: String,
     email: String,
     numeroTelefone: String,
     anoNascimento: String
 })
+
+mongoose.model("people", userSchema)
