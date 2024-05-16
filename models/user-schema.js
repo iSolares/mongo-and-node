de4209ed0cb0f2e9e5e9a5fc9c7f6c91f6025a7e
Moisés Solares Santos <moisessolaressantos@gmail.com>
2024-05-16 12:49:28 -0300

    Revert "make a better form"
    This reverts commit 663a8d92aeacef9493fe3732290778d422f915aa.

--- models/user-schema.js
+++ models/user-schema.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose')
 
- const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({
     _id: mongoose.Schema.ObjectId,
     nome: String,
     email: String,
     numeroTelefone: String,
     anoNascimento: String
 })
-
-mongoose.model("people", userSchema)
